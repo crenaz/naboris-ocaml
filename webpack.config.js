2fb32699c5c87c61aad614b25d69c02df195d0da
crenaz <crenaz@gmail.com>
2025-04-16 20:54:52 -0400

    854

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, 'lib/js/clientSrc/Main.js'),
-    output: {
-        path: path.resolve(__dirname, 'public/js/dist'),
-        filename: 'main.js',
-    },
+  mode: "production",
+  entry: path.resolve(__dirname, "lib/js/clientSrc/Main.bs.js"),
+  output: {
+    path: path.resolve(__dirname, "public/js/dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js", ".bs.js"],
+  },
 };
